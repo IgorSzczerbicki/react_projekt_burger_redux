c049c442606f2b27cd939d448cab829c5826757d
Pl4cek <pl4cek@outlook.com>
2018-08-12 11:20:43 +0200

    Reducer do składników

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,13 +27,13 @@ class BurgerBuilder extends Component {
 	};
 
 	componentDidMount () {
-		axios.get('https://react-projekt-burger.firebaseio.com/ingredients.json')
-			.then(response => {
-				this.setState({ingredients: response.data})
-			})
-			.catch(error => {
-				this.setState({error: true})
-			})
+		// axios.get('https://react-projekt-burger.firebaseio.com/ingredients.json')
+		// 	.then(response => {
+		// 		this.setState({ingredients: response.data})
+		// 	})
+		// 	.catch(error => {
+		// 		this.setState({error: true})
+		// 	})
 	}
 
 	updatePurchaseState(ingredients) {
